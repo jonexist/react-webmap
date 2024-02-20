35424529e8f62e754b47f97c754a3b0f437e33df
Jonexist <79012225+jonexist@users.noreply.github.com>
2024-02-20 10:24:45 +0800

    Refactor fetchGeocodingApi function and add comments

--- src/lib/fetchGeocodingApi.js
+++ src/lib/fetchGeocodingApi.js
@@ -3,21 +3,25 @@ import icon from '../assets/location-pin.png';
 import { createCustomMarker } from '../utils/createCustomMarker';
 import { addPopupToMarker } from '../utils/addPopupToMarker';
 
-const constructApiUrl = (lng, lat, radius, placeType, token) =>
-  `https://api.mapbox.com/geocoding/v5/mapbox.places/${placeType}.json?proximity=${lng},${lat}&radius=${radius}&access_token=${token}`;
+// Function to construct the URL for the MapBox Geocoding API request
+const constructApiUrl = (lng, lat, placeType, token) =>
+  `https://api.mapbox.com/geocoding/v5/mapbox.places/${placeType}.json?proximity=${lng},${lat}&access_token=${token}`;
 
+// Function to process each location from the API response
 const processLocation = (pharmacyLoc, map, markersRef) => {
+  // Extract the coordinates from the location's geometry
   const { coordinates } = pharmacyLoc.geometry;
-
+  // Create a custom marker using the provided icon
   const el = createCustomMarker(icon);
-
+  // Create a new marker at the location's coordinates and add it to the map
   const marker = new mapboxgl.Marker(el).setLngLat(coordinates).addTo(map);
-
+  // Add the new marker to the markers reference array
   markersRef.current.push(marker);
-
+  // Add a popup to the marker with the location's information
   addPopupToMarker(marker, pharmacyLoc, map);
 };
 
+// Function to fetch data from MapBox GL JS Geocoding API
 const fetchGeocodingApi = async (
   mapRef,
   locationCoordinates,
@@ -26,18 +30,20 @@ const fetchGeocodingApi = async (
   abortController
 ) => {
   const [lng, lat] = locationCoordinates;
-  const radius = 20000;
-  const placeType = 'pharmacy';
-
-  const url = constructApiUrl(lng, lat, radius, placeType, token);
-
+  // Construct the URL for the API request using the given longitude, latitude, and place type
+  const url = constructApiUrl(lng, lat, 'pharmacy', token);
+  // Remove the previous markers from the map
   markersRef.current.forEach((marker) => marker.remove());
+  // Reset the markers reference array
   markersRef.current = [];
 
   try {
+    // Fetch data from the API
     const response = await fetch(url, { signal: abortController.signal });
+    // Parse the response as JSON
     const data = await response.json();
-
+    // Process each location from the API response
+    // This involves adding a marker to the map for each location
     data.features.forEach((pharmacyLoc) =>
       processLocation(pharmacyLoc, mapRef, markersRef)
     );
