0d36a4deae51bdc2433e647603fcbad16871dcde
Jonexist <79012225+jonexist@users.noreply.github.com>
2024-02-20 10:22:39 +0800

    Refactor code and add important comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './index.css';
 import { useRef, useEffect, useState, useMemo } from 'react';
 import mapboxgl from 'mapbox-gl';
-import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
-import { addPopupToMarker } from './utils/addPopupToMarker';
 import fetchGeocodingApi from './lib/fetchGeocodingApi';
+import addSearchControl from './utils/addSearchControl';
+import getUserLocation from './utils/getUserLocation';
+import addDefaultMarker from './utils/addDefaultMarker';
 
 mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
 
@@ -19,31 +20,25 @@ const App = () => {
     zoom: 12,
   });
 
+  // Memoize the locationProximity value to prevent unnecessary re-renders
   const locationProximity = useMemo(
     () => [mapState.lng, mapState.lat],
     [mapState.lat, mapState.lng]
   );
 
   useEffect(() => {
+    // If the map is already initialized, exit early
     if (mapRef.current) return;
-
+    // Initialize a new Mapbox map
     const map = new mapboxgl.Map({
       container: mapContainerRef.current,
       style: 'mapbox://styles/mapbox/streets-v12',
       center: [mapState.lng, mapState.lat],
       zoom: mapState.zoom,
     });
-
-    const currentMarker = new mapboxgl.Marker({ color: 'red' })
-      .setLngLat(locationProximity)
-      .addTo(map);
-
-    addPopupToMarker(
-      currentMarker,
-      { place_name: 'Concord, North Carolina, United States' },
-      map
-    );
-
+    // Add a default marker to the map at the current location
+    addDefaultMarker(map, locationProximity);
+    // Update the map state whenever the map moves
     map.on('move', () => {
       setMapState({
         lng: map.getCenter().lng.toFixed(4),
@@ -52,12 +47,28 @@ const App = () => {
       });
     });
 
+    // Store the map instance in a ref
     mapRef.current = map;
   }, [mapState, locationProximity]);
 
   useEffect(() => {
-    const abortController = new AbortController();
+    // If the map is not initialized yet, exit early
+    if (!mapRef.current) return;
 
+    const map = mapRef.current;
+    // Once the map has loaded, add the search and geolocate controls if they haven't been added yet
+    map.on('load', () => {
+      if (!geocoderRef.current && !geolocateRef.current) {
+        addSearchControl(map, mapboxgl.accessToken, geocoderRef);
+        getUserLocation(map, geolocateRef);
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    // Create an AbortController for the fetch request
+    const abortController = new AbortController();
+    // If the map is initialized, fetch geocoding data
     if (mapRef.current) {
       fetchGeocodingApi(
         mapRef.current,
@@ -67,56 +78,11 @@ const App = () => {
         abortController
       );
     }
-
+    // When the locationProximity changes, abort the fetch request
     return () => {
       abortController.abort();
     };
   }, [locationProximity]);
-
-  useEffect(() => {
-    if (!mapRef.current) return;
-
-    const map = mapRef.current;
-
-    const addGeocoderControl = () => {
-      const geocoder = new MapboxGeocoder({
-        accessToken: mapboxgl.accessToken,
-        mapboxgl: mapboxgl,
-        marker: true,
-      });
-
-      map.addControl(geocoder);
-      geocoderRef.current = geocoder;
-    };
-
-    const addGeolocateControl = () => {
-      const geolocate = new mapboxgl.GeolocateControl({
-        positionOptions: {
-          enableHighAccuracy: true,
-        },
-        trackUserLocation: true,
-      });
-
-      const marker = new mapboxgl.Marker();
-
-      geolocate.on('geolocate', (e) => {
-        const { longitude, latitude } = e.coords;
-        marker.setLngLat([longitude, latitude]).addTo(map);
-      });
-
-      addPopupToMarker(marker, { place_name: 'Your current location' }, map);
-
-      map.addControl(geolocate);
-      geolocateRef.current = geolocate;
-    };
-
-    map.on('load', () => {
-      if (!geocoderRef.current && !geolocateRef.current) {
-        addGeocoderControl();
-        addGeolocateControl();
-      }
-    });
-  }, []);
 
   return (
     <>
