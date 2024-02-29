cd09ccf8069c7cee169149abe133b81d4aec3d29
Jonexist <79012225+jonexist@users.noreply.github.com>
2024-02-29 20:56:30 +0800

    Add distance to marker popup

--- src/utils/addPopupToMarker.js
+++ src/utils/addPopupToMarker.js
@@ -1,9 +1,12 @@
 import mapboxgl from 'mapbox-gl';
 import { addPopupEvents } from '../utils/addPopupEvents';
 
-export const addPopupToMarker = (marker, pharmacyLoc, map) => {
+export const addPopupToMarker = (marker, pharmacyLoc, map, distance) => {
   const popup = new mapboxgl.Popup({ offset: 25 })
-    .setText(pharmacyLoc.place_name)
+    .setText(
+      `${pharmacyLoc.place_name}` +
+        (distance ? `\nDistance: ${distance.toFixed(2)} km` : '')
+    )
     .addTo(map);
 
   addPopupEvents(marker, popup, map);
